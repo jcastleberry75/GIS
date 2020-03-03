a78d44170197f39148a6447c509b945d0b2ec91e
Jay Castleberry <jcastleberry75@outlook.com>
2020-03-03 14:51:53 -0500

    Create rest_api.py

---
+++ rest_api.py
@@ -0,0 +1,42 @@
+from flask import Flask, request
+from custom_logger import cus_logger
+
+
+logger = cus_logger("zoom_tests.log")
+
+def error_handler(error):
+    error_msg = str(error)
+    logger.critical("The Flask Server has generated the following error")
+    logger.debug(error_msg)
+
+
+def main():
+    logging.info('#################### FLASK SERVER STARTED  ####################')
+    app = Flask(__name__)
+
+    @app.route("/testing", methods=['POST'])
+    def test():
+        try:
+            if request.method == 'POST':
+                incoming_msg = request.args.get('msg')
+                if incoming_msg:
+                    logger.info(incoming_msg)
+                    rsp = f"""Message Received: {incoming_msg}"""
+                    logger.info(rsp)
+                    return(rsp)
+                else:
+                    logger.warning(f"""Bad POST: {request}""")
+                    return ("Go Away")
+            else:
+                logger.warning(f"""Bad POST: {request}""")
+                return("Go Away")
+
+        except (OSError, SystemError, ConnectionError, Exception) as err:
+            error_handler(err)
+
+    while True:
+        app.run(host='0.0.0.0', debug=True, port=10001)
+
+
+if __name__ == "__main__":
+    main()
